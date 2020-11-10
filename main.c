1259e865710f2021f5e06aea959fe6989b950f9f
hgo641 <65850682+hgo641@users.noreply.github.com>
2020-11-10 21:17:01 +0900

    update git ignore for visual studio

--- main.c
+++ main.c
@@ -1,9 +1,48 @@
-#include <studio.h>
+#include <stdio.h>
 #include <string.h>
-//strcpy strcat strcmp
 
-int strcmp(char* dest, char* src) {
-	if (strlen(dest))
+/*
+	strlng없이 구현
+	
+	헤더 사용 - 함수 선언
+	메뉴 출력
+	도서 찾기
+	도서 업데이트
+*/
+char* Strcpy(char* dest, char* src)
+{
+	int i = 0;
+	while (src[i] != '\0') {
+		dest[i] = src[i];
+		i++;
+	}
+	return dest;
+}
+int str_cmp(char* a , char* b) {
+	int i = 0;
+	while (i!=NULL) {
+		if (a[i] > b[i])
+			return -1;
+		else if (a[i] < b[i])
+			return 1;
+		i++;
+	}
+	return 0;
+}
+
+int Strcmp(const char* s1, const char* s2)
+{
+	unsigned char c1, c2;
+
+	while (1) {
+		c1 = *s1++; 
+		c2 = *s2++;
+		if (c1 != c2)
+			return c1 < c2 ? -1 : 1;
+		if (!c1)
+			break;
+	}
+	return 0;
 }
 
 char* Strcat(char* dest, char* src)
@@ -16,8 +55,10 @@ char* Strcat(char* dest, char* src)
 }
 void main() {
 	
-	char str1[10] = { "hel" };
-	char str2[10] = { "hello" };
-	printf(Strcmp(str1, str2));
+	char str1[10] = "hel";
+	char str2[10] = "hello";
+
+	
+	printf("strcmp(\"%s,%s\"): %d",str1, str2, Strcmp(str1, str2));
 
 }
